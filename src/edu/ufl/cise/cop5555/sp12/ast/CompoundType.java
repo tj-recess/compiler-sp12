c248d0b932306f1b90f18a74f0c192551a0313ac
arpit1712@gmail.com <arpit1712@gmail.com@e6588041-72de-8b1c-3bbe-552f94377197>
2012-02-28 15:20:39 +0000

    Initial Upload for AST (project 3) and some additional test cases for P2.

---
+++ src/edu/ufl/cise/cop5555/sp12/ast/CompoundType.java
@@ -0,0 +1,18 @@
+package edu.ufl.cise.cop5555.sp12.ast;
+
+public class CompoundType extends Type {
+	
+	public final SimpleType keyType;
+	public final Type valType;
+
+	public CompoundType(SimpleType keyType, Type valType) {
+		this.keyType = keyType;
+		this.valType = valType;
+	}
+
+	@Override
+	public Object visit(ASTVisitor v, Object arg) throws Exception {
+		return v.visitCompoundType(this, arg);
+	}
+
+}
---
+++ src/edu/ufl/cise/cop5555/sp12/ast/SimpleType.java
@@ -0,0 +1,18 @@
+package edu.ufl.cise.cop5555.sp12.ast;
+
+import edu.ufl.cise.cop5555.sp12.Kind;
+
+public class SimpleType extends Type {
+	
+	Kind type;
+	
+	public SimpleType(Kind type){
+		this.type = type;
+	}
+
+	@Override
+	public Object visit(ASTVisitor v, Object arg) throws Exception {
+		return v.visitSimpleType(this,arg);
+	}
+
+}

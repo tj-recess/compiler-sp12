4e745e53e4b81228e30278a1aab127c8b5f1f5ec
arpit1712@gmail.com <arpit1712@gmail.com@e6588041-72de-8b1c-3bbe-552f94377197>
2012-02-29 06:26:37 +0000

    Coding phase done. Testing phase hasn't started yet. Refactored old test cases to be compatible with code changes.

--- test/edu/ufl/cise/cop5555/sp12/TestSimpleParser.java
+++ test/edu/ufl/cise/cop5555/sp12/TestSimpleParser.java
@@ -1,13 +1,6 @@
 package edu.ufl.cise.cop5555.sp12;
 
-import static edu.ufl.cise.cop5555.sp12.Kind.*;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-
-import edu.ufl.cise.cop5555.sp12.Scanner;
-import edu.ufl.cise.cop5555.sp12.SimpleParser;
-import edu.ufl.cise.cop5555.sp12.TokenStream;
 
 public class TestSimpleParser {
    
@@ -20,227 +13,227 @@ public class TestSimpleParser {
 	
 	
 	@Test
-	public void testEmptyProg(){
+	public void testEmptyProg() throws SyntaxException
+	{
 		String input = "prog Test1 gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 	}
 	
 	@Test
-	public void testIntDec(){
+	public void testIntDec() throws SyntaxException
+	{
 		String input = "prog Test1 int x; gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 	}
 	
 	@Test
-	public void testBooleanDec(){
+	public void testBooleanDec() throws SyntaxException
+	{
 		String input = "prog Test1 boolean x; gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 	}
 	
 	@Test
-	public void testMapDec(){
+	public void testMapDec() throws SyntaxException
+	{
 		String input = "prog Test1 map[int,string] y; gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 	}
 	
 	@Test
-	public void testMapDec2(){
+	public void testMapDec2() throws SyntaxException
+	{
 		String input = "prog Test1 map[int,map[string,boolean]] m; gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 	}
 	
-	@Test
-	public void xtestMapDec2(){
+	@Test (expected = SyntaxException.class)
+	public void xtestMapDec2() throws SyntaxException{
 		String input = "prog Test1 map[int,map[string,boolean]] ; gorp";  //semi is the error, should be ident
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNotNull(result);
-		assertEquals(SEMI, result.t.kind);
+		parser.parse();
+//		assertNotNull(result);
+//		assertEquals(SEMI, result.t.kind);
 	}
 	
-	@Test
-	public void Test1()
+	@Test(expected = SyntaxException.class)
+	public void Test1() throws SyntaxException
 	{
 		String input = "prog gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNotNull(result);
-		
+		parser.parse();		
 	}
 	
-	@Test
-	public void Test2()
+	@Test(expected = SyntaxException.class)
+	public void Test2() throws SyntaxException
 	{
 		String input = "gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNotNull(result);
-		
+		parser.parse();		
 	}
 	
 	@Test
-	public void Test3()
+	public void Test3() throws SyntaxException
 	{
 		String input = "prog Test ; gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 		
 	}
 	@Test
-	public void Test4()
+	public void Test4() throws SyntaxException
 	{
 		String input = "prog Test int a; int b; ; ;gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 		
 	}
-	@Test
-	public void Test5()
+	@Test(expected = SyntaxException.class)
+	public void Test5() throws SyntaxException
 	{
 		String input = "prog Test a[true] = truegorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNotNull(result);
-		
+		parser.parse();		
 	}
-	@Test
-	public void Test6()
+	
+	@Test(expected = SyntaxException.class)
+	public void Test6() throws SyntaxException
 	{
 		String input = "";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNotNull(result);
+		parser.parse();
 		
 	}
 	@Test
-	public void Test7()
+	public void Test7() throws SyntaxException
 	{
 		String input = "prog Test a[true] = true; ;;;gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 		
 	}
 	
 	@Test
-	public void Test8()
+	public void Test8() throws SyntaxException
 	{
 		String input = "prog Test int a; boolean b;string s;map[int,int] m; map[int,map[int , boolean ] ] m; map[int,map[boolean,map[string,string]]] m;gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 		
 	}
 	
 	@Test
-	public void Test9()
+	public void Test9() throws SyntaxException
 	{
 		String input = "prog Test a = b & b -2; print \"abc\"+2;gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 		
 	}
 	@Test
-	public void Test10()
+	public void Test10() throws SyntaxException
 	{
 		String input = "prog Test if(2* \"abc\") ; fi ;gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 		
 	}
 	
 	@Test
-	public void Test11()
+	public void Test11() throws SyntaxException
 	{
 		String input = "prog Test if(2* \"abc\") ; else ; fi ;gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 		
 	}
 	
 	@Test
-	public void Test12()
+	public void Test12() throws SyntaxException
 	{
 		String input = "prog Test int a; boolean b; string c; do b : [a,b] od; gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);
+		parser.parse();
+		
 		
 	}
 	
 	@Test
-	public void Test13()
+	public void Test13() throws SyntaxException
 	{
 		String input = "prog Test do (2* 5 + a[\"abc\"] == true ) od;gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);	
+		parser.parse();
+			
 		
 	}
 	
 	@Test
-	public void Test14()
+	public void Test14() throws SyntaxException
 	{
 		String input = "prog Test a[2/ \"a\"] = {} ;gorp";
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);	
+		parser.parse();
+			
 		
 	}
 	@Test
-	public void Test15()
+	public void Test15() throws SyntaxException
 	{
 		String input = "prog Test a[(!2)] = {[2,false] , [2,2] };gorp";
 
 		TokenStream stream = getInitializedTokenStream(input);
 		SimpleParser parser = new SimpleParser(stream);
-		SyntaxException result = parser.parse();
-		assertNull(result);	
+		parser.parse();
+			
 		
 	}
 	
 	@Test
-	public void xtestMapDec28(){
+	public void xtestMapDec28() throws SyntaxException
+	{
         String input = "prog main int x;boolean y;map[string,string] mapHash;x=12;x=12*3;y={};y={[x+y,10/12]};y[10]=20; print hello; println xyz*3;do(x>y) string new;new=\"Hello World\"; od;  do x:[y,z] y=y+z*20; od; if(g>s) mapHash[x]=y; fi; if(g<=s) mapHash[y]=x; else mapHash[x]=y; fi; x=x & !y; x=x-y; x=(x-y*20)/20; gorp";  //This should pass without any errors
         TokenStream stream = getInitializedTokenStream(input);
         SimpleParser parser = new SimpleParser(stream);
-        SyntaxException result = parser.parse();
-        assertNull(result);
+        parser.parse();
+        
     
         }
 }

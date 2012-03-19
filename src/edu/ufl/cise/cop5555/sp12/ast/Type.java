9d32d46e270f68283fc17c0efd72f96f69d27343
arpit1712@gmail.com <arpit1712@gmail.com@e6588041-72de-8b1c-3bbe-552f94377197>
2012-03-19 23:38:29 +0000

    All done except DoEach. Need TA's help before that could be done. Overridden equals method in Type, SimpleType and CompoundType.

--- src/edu/ufl/cise/cop5555/sp12/ast/Type.java
+++ src/edu/ufl/cise/cop5555/sp12/ast/Type.java
@@ -2,4 +2,6 @@ package edu.ufl.cise.cop5555.sp12.ast;
 
 public abstract class Type extends AST
 {
+    @Override
+    public abstract boolean equals(Object anotherType);
 }
--- src/edu/ufl/cise/cop5555/sp12/context/SymbolTable.java
+++ src/edu/ufl/cise/cop5555/sp12/context/SymbolTable.java
@@ -1,36 +1,111 @@
 package edu.ufl.cise.cop5555.sp12.context;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 import edu.ufl.cise.cop5555.sp12.ast.Declaration;
 
-public class SymbolTable {
+public class SymbolTable
+{
+    private class SymbolTableEntry
+    {
+        private int scope;
+        private Declaration declaration;
+        
+        public SymbolTableEntry(int scope, Declaration declaration)
+        {
+            this.scope = scope;
+            this.declaration = declaration;
+        }
+        
+        public int getScope()
+        {
+            return scope;
+        }
 
-
-	public SymbolTable(){
-     //TODO Implement me
+        public Declaration getDeclaration()
+        {
+            return declaration;
+        }
+    }
+    
+    private Stack<Integer> scopeStack;
+    private int currentScope = 0;   //important to keep it 0
+    private HashMap<String, List<SymbolTableEntry>> symbolTable;
+    
+	public SymbolTable()
+	{
+	    this.scopeStack = new Stack<Integer>();
+	    this.symbolTable = new HashMap<String, List<SymbolTableEntry>>();
 	}
 
 
-	public void enterScope() {
-		//TODO Implement me
+	public void enterScope()
+	{
+		this.scopeStack.push(currentScope++);
 	}
 
-	public void exitScope() {
-		//TODO Implement me
+	public void exitScope()
+	{
+	    this.scopeStack.pop();
 	}
 
-	// returns the in-scope declaration of the name if there is one, 
-	//otherwise it returns null
-	public Declaration lookup(String ident) {
-    //TODO Implement me
+	/**
+	 * returns the in-scope declaration of the name if there is one,
+	 * otherwise it returns null
+	 * @param ident identifier to be looked up
+	 * @return Declaration corresponding to the identifier
+	 */
+	public Declaration lookup(String ident) 
+	{
+	    List<SymbolTableEntry> matchingList = this.symbolTable.get(ident);
+	    if(matchingList == null)
+	        return null;
+	    
+        SymbolTableEntry bestEntry = null;	    
+	    for(int i = scopeStack.size() - 1; i >= 0; i--)    //start from the top to bottom
+        {
+	        int bestScope = scopeStack.get(i);
+	        for(SymbolTableEntry anEntry : matchingList)
+	        {
+	            if(anEntry.getScope() == bestScope && bestEntry == null)
+	            {
+	                bestEntry = anEntry;
+	                return bestEntry.getDeclaration();  //found the best entry, there is no other possibility
+	            }
+	        }
+	    }
 	    return null;
 	}
-	
 
-	// if the name is already declared IN THE CURRENT SCOPE, returns false. 
-	//Otherwise inserts the declaration in the symbol table
-	public boolean insert(String ident, Declaration dec) {
-	//TODO Implement me
-	    return false;
+	/**
+	 * If the name is already declared IN THE CURRENT SCOPE, returns false.
+	 * Otherwise inserts the declaration in the symbol table
+	 * @return false If the name is already declared in the CURRENT SCOPE
+	 */
+	public boolean insert(String ident, Declaration dec) 
+	{
+	    List<SymbolTableEntry> existingList = this.symbolTable.get(ident);
+	    if(existingList == null)
+	    {
+	        existingList = new LinkedList<SymbolTableEntry>();
+	    }
+	    else
+	    {
+	        for(SymbolTableEntry anEntry : existingList)
+	        {
+	            if(anEntry.getScope() == scopeStack.peek())
+	            {
+	                return false;
+	            }
+	        }
+	    }
+	    //always insert at 0th index, avoid going to the end of list 
+	    //(just in case "tail" pointer isn't maintained
+	    existingList.add(0, new SymbolTableEntry(scopeStack.peek(), dec));
+	    return true;
 	}
 
 
